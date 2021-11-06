c3dd533b0fd2bed6d0496c85fc22acc5dc0f6f8b
tkaryadis <tkaryadis@gmail.com>
2021-11-06 03:52:36 +0200

    "move docs to seperate website"

--- docs/7b4c6a5e.f7952750.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[28],{87:function(o){o.exports=JSON.parse('{"allTagsPath":"/cmql-core/blog/tags","slug":"facebook","name":"facebook","count":1,"permalink":"/cmql-core/blog/tags/facebook"}')}}]);
